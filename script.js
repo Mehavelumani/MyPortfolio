4ce9caf30212209e3570f49fb49248de0099b3f6
MEHA <mehavs2002@gmail.com>
2023-08-29 13:49:56 +0530

    adding all files to git repo

---
+++ script.js
@@ -0,0 +1,16 @@
+// You can add JavaScript interactions here if needed
+// For example, smooth scrolling to sections on navigation link clicks
+document.addEventListener('DOMContentLoaded', function() {
+    const navLinks = document.querySelectorAll('nav a');
+    
+    navLinks.forEach(link => {
+        link.addEventListener('click', smoothScroll);
+    });
+    
+    function smoothScroll(e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href');
+        const targetSection = document.querySelector(targetId);
+        targetSection.scrollIntoView({ behavior: 'smooth' });
+    }
+});
